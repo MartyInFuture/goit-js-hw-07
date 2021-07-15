52e36fa77323de08549c3031ee3da50e6acde77b
MartyInFuture <martyinfuture@gmail.com>
2021-07-15 17:38:41 +0300

    add fix

--- js/task-6.js
+++ js/task-6.js
@@ -2,7 +2,7 @@ const input = document.querySelector('input#validation-input');
 const dataLength = input.getAttribute('data-length');
 
 input.addEventListener('focusout', (e) => {
-  if (e.target.value.length < dataLength) {
+  if (e.target.value.length != dataLength) {
     input.classList.add('invalid');
     input.classList.remove('valid');
   } else {
